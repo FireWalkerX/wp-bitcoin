e53379edc3a5d6b03e6b0f398ef02d3a19b2ce5d
mra13 <mra13@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-10 01:28:01 +0000

    committing v1.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-bitcoin/trunk@854493 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ general-settings.php
@@ -0,0 +1,85 @@
+<?php
+function wp_bitcoin_display_general_settings_menu()
+{
+    $config = WP_Bitcoin_Config::getInstance();
+    if(isset($_POST['wp_bitcoin_settings_update'])){
+        $config->setValue('wpbc_bitpay_enabled',($_POST["wpbc_bitpay_enabled"]=='1')?1:'');
+        $config->setValue('wpbc_bitpay_api_key',trim($_POST["wpbc_bitpay_api_key"]));
+        $config->setValue('wpbc_bitpay_transaction_speed',trim($_POST["wpbc_bitpay_transaction_speed"]));
+        $config->setValue('wpbc_bitpay_form_page_url',trim($_POST["wpbc_bitpay_form_page_url"]));
+        $config->setValue('wpbc_bitpay_form_page_title',trim($_POST["wpbc_bitpay_form_page_title"]));				
+        echo '<div class="updated"><p>Settings updated!</p></div>'; 
+        $config->saveConfig();
+            
+    }
+
+?>
+
+    <div class="postbox">
+    <h3><label for="title">Usage Instruction</label></h3>
+    <div class="inside">
+    <p>You have successfully installed the WordPress Bitcoin plugin.</p>
+    <p><strong>Step 1)</strong> Enter your bitpay credentials in the "Settings" section</p>
+    <p><strong>Step 1)</strong> Insert a shortcode for your product to create a Buy Now button:</p>
+    <p><code>[wpbc_buy_now item_name="test product" price="5.00" currency="USD"]</code></p>
+    <p>Check the <a href="http://www.tipsandtricks-hq.com/wordpress-bitcoin-payment-plugin" target="_blank">WP Bitcoin plugin page</a> for more information</p>
+    </div></div>
+
+    <div class="postbox">
+    <h3><label for="title">Bitpay Settings</label></h3>
+    <div class="inside">
+
+    <form method="post" action="">
+    <table class="form-table" width="100%" border="0" cellspacing="0" cellpadding="6">
+
+    <tr valign="top"><td width="25%" align="left">
+    Enable Bitpay
+    </td><td align="left">
+    <input name="wpbc_bitpay_enabled" type="checkbox"<?php if($config->getValue('wpbc_bitpay_enabled')!='') echo ' checked="checked"'; ?> value="1"/>
+    <p class="description">Check this to enable Bitpay gateway.</p>
+    </td></tr>
+
+    <tr valign="top"><td width="25%" align="left">
+    API Key
+    </td><td align="left">
+    <input name="wpbc_bitpay_api_key" type="text" size="80" value="<?php echo $config->getValue('wpbc_bitpay_api_key'); ?>"/>
+    <p class="description">Your Bitpay merchant api key</p>
+    </td></tr>
+    
+    <tr valign="top"><td width="25%" align="left">
+    Transaction Speed
+    </td><td align="left">
+    <?php $txn_speed = $config->getValue('wpbc_bitpay_transaction_speed');?>
+    <select name="wpbc_bitpay_transaction_speed" >
+        <option <?php echo ($txn_speed==='low')?'selected="selected"':'';?> value="low">Low</option>
+        <option <?php echo ($txn_speed==='medium')?'selected="selected"':'';?> value="medium">Medium</option>
+        <option <?php echo ($txn_speed==='high')?'selected="selected"':'';?> value="high">High</option>
+    </select>
+        <br /><i>Speed at which the bitcoin transaction registers as "confirmed" to the store. This overrides your merchant settings on the Bitpay website</i><br /><br />
+    </td></tr>
+    
+    <tr valign="top"><td width="25%" align="left">
+    Order Information Page
+    </td><td align="left">
+    <input name="wpbc_bitpay_form_page_url" type="text" size="80" value="<?php echo $config->getValue('wpbc_bitpay_form_page_url'); ?>"/>
+    <br /><i>URL of the page where order information will be collected from your users.</i>
+    </td></tr>
+    
+    <tr valign="top"><td width="25%" align="left">
+    Order Information Page Title
+    </td><td align="left">
+    <input name="wpbc_bitpay_form_page_title" type="text" size="80" value="<?php echo $config->getValue('wpbc_bitpay_form_page_title'); ?>"/>
+    <br /><i>Title of the page where order information will be collected from your users.</i>
+    </td></tr>
+    
+    </table>
+    
+    <div class="submit">
+        <input type="submit" name="wp_bitcoin_settings_update" class="button-primary" value="Update" />
+    </div>
+     
+    </form>
+    </div></div>
+	
+<?php
+}
